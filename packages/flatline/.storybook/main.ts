58de9f7354f3a1106ac0900245e16fc57e89a671
juanka <juanka.kode@gmail.com>
2025-12-10 23:53:10 -0600

    build: init repo

---
+++ packages/flatline/.storybook/main.ts
@@ -0,0 +1,9 @@
+import { defineMain } from '@storybook/react-vite/node';
+
+export default defineMain({
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [],
+  "framework": "@storybook/react-vite"
+});
---
+++ packages/flatline/.storybook/preview.ts
@@ -0,0 +1,6 @@
+import { definePreview } from '@storybook/react-vite';
+import '../src/flatline.css';
+
+export default definePreview({
+  addons: []
+});
